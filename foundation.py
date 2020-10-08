3dff9e9e8388de0b3ae2091c1f5e7622c0ce3886
Christian Iosif <iosif.christian@gmail.com>
2020-10-08 18:58:17 -0400

    Sends and receives request for loading project cards

--- foundation.py
+++ foundation.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+import json, requests
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return {'statement' : 'Hello World'}
+@app.route('/project', methods=['GET'])
+def get_projects():
+    category = request.args.get('category')
+    num_recs = request.args.get('num_recs')
+    index = request.args.get('index')
+
+    payload = {'category': category, 'num_recs': num_recs, 'index': index}
+
+    r = requests.get('http://127.0.0.1:8001/project', params=payload)
+    response = r.text
+    
+    return response
