e708340eac21865ed181892146f4629e6e7cb356
Christian Iosif <iosif.christian@gmail.com>
2020-10-09 17:55:50 -0400

    Simplified the API and added global oven URL

--- foundation.py
+++ foundation.py
@@ -2,15 +2,13 @@ from flask import Flask, request
 import json, requests
 app = Flask(__name__)
 
+oven_url = 'http://127.0.0.1:8001/project'
+
 @app.route('/project', methods=['GET'])
 def get_projects():
-    category = request.args.get('category')
-    num_recs = request.args.get('num_recs')
-    index = request.args.get('index')
+    args = request.args
 
-    payload = {'category': category, 'num_recs': num_recs, 'index': index}
-
-    r = requests.get('http://127.0.0.1:8001/project', params=payload)
+    r = requests.get(oven_url, params=args)
     response = r.text
     
     return response
