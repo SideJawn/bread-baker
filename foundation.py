c8f616ff9d4e39ce804a3c27a3aee768f1aa74ca
Christian Iosif <iosif.christian@gmail.com>
2020-10-11 12:23:53 -0400

    Able to create user, created reusable oven method

--- foundation.py
+++ foundation.py
@@ -1,14 +1,64 @@
 from flask import Flask, request
-import json, requests
+import json, requests, string, secrets, hashlib
 app = Flask(__name__)
 
-oven_url = 'http://127.0.0.1:8001/project'
+oven_url = 'http://127.0.0.1:8001'
 
+#Middleware for data in project cards feed
 @app.route('/project', methods=['GET'])
 def get_projects():
-    args = request.args
-
-    r = requests.get(oven_url, params=args)
-    response = r.text
+    params = request.args
+    response = send_to_oven('/project', 'GET', params)
     
+    return response
+
+#Creates a hashed salted password for new users
+@app.route('/user', methods=['PUT'])
+def create_user():
+    user = request.get_json()['data']
+    user_pass = ''
+    alphabet = string.ascii_letters + string.digits
+
+    if user['password'] is not None:
+        user_pass = user['password']
+        salt = ''.join(secrets.choice(alphabet) for i in range(8))
+        salted_pass = user_pass + salt
+        hashed_pass = hashlib.sha256(salted_pass.encode()).hexdigest()
+
+        new_creds = {'salt': salt, 'hashed_pass': hashed_pass}
+        del user['password']
+        user.update(new_creds)
+        oven_ready_dict = {'data': user}
+
+        response = send_to_oven('/user', 'PUT', oven_ready_dict)
+    else:
+        response = { 'status_code': 'Password is missing' }
+
+    return response
+
+#Communicates with oven API
+def send_to_oven(endpoint, request_type, payload):
+    updated_oven_url = oven_url + endpoint
+    r = ''
+    try:
+        if request_type == 'PUT':
+            header = {"Content-Type": "application/json"}
+            json_payload = json.dumps(payload)
+            r = requests.put(updated_oven_url, headers= header, data=json_payload)
+        elif request_type == 'GET':
+            r = requests.get(updated_oven_url, params=payload)
+        response = r.json()
+    except requests.ConnectionError as err:
+        print(err)
+        response = { 'status_code': 'Connection Error'}
+    except requests.exceptions.HTTPError as err:
+        print(err)
+        response = { 'status_code': 'HTTP Error'}
+    except requests.exceptions.Timeout as err:
+        print(err)
+        response = { 'status_code': 'Timeout Error'}
+    except requests.exceptions.RequestException as err:
+        print(err)
+        response = { 'status_code': 'General error'}
+
     return response
